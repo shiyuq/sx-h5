d520b723f621ce51ed4c63d190991e811d3f5aa3
xenialyj <xenia310lyj@gmail.com>
2021-09-07 07:34:06 +0800

    公司动态+宽度调整（存在问题）

--- src/views/home/components/gongsixinwen/component.js
+++ src/views/home/components/gongsixinwen/component.js
@@ -1,7 +1,18 @@
 export default {
   data() {
     return {
-      active: 2
+      iscur: 0,
+      autoplay: true,
+      cList: [
+        {
+          // src: '/gongsixinwen/xingyedongtai/20181126/158.html',
+          title: '习近平在深入推动长江经济带发展座谈会上的讲',
+          // imgUrl: require('./img/xijinping.jpg'),
+          content: '这次座谈会是我主持召开的第二次长江经济带发展座谈会。上一次是2016年1月在长江上游的重庆召开，这一次放在长江中游的武汉召开。 推动长江经济带发展是党中央作出的重大决策，...',
+          subSrc: '/gongsixinwen/xingyedongtai/20181124/131.html',
+          subTitle: '习近平在中阿合作论坛第八届部长级会议开幕式'
+        }
+      ]
     }
   },
 
