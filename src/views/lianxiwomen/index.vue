09d1b95ac4c00faac489c4483450b501097f388c
xenialyj <xenia310lyj@gmail.com>
2021-09-27 06:31:32 +0800

    关于我们完成，联系我们未完成

--- src/views/lianxiwomen/index.vue
+++ src/views/lianxiwomen/index.vue
@@ -1,7 +1,27 @@
 <!-- home -->
 <template>
-  <div>
-    ceshi ceshi
+  <div class="home-lianxiwomen">
+    <van-nav-bar  left-text="返回" @click-left="gotoMenu"    title="关于我们" left-arrow>
+      <template #right>
+        <van-icon name="bars" size="18" />
+      </template>
+    </van-nav-bar>
+    <div class="lianxi">
+      <img class="icon" :src="require('./images/address.png')">
+      地址：新疆阿拉尔市秋收大道1001号
+    </div>
+    <div class="lianxi">
+       <img class="icon" :src="require('./images/tele.png')">
+    电话： 0991-6378522 15083931223
+    </div>
+    <div class="lianxi">
+       <img class="icon" :src="require('./images/mail.png')">
+邮箱：364361414@qq.com
+    </div>
+    <img :src="require('./images/map.jpg')"/>
+     <form action="/">
+      <van-search v-model="value" show-action placeholder="请输入搜索关键词" @search="onSearch" @cancel="onCancel"></van-search>
+    </form>
   </div>
 </template>
 
