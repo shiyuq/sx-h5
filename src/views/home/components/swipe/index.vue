b73ec062bf95aa29e239d5dd904f111b721538f2
shiyuq <1543082564@qq.com>
2021-09-28 16:49:29 +0800

    背景图修改

--- src/views/home/components/swipe/index.vue
+++ src/views/home/components/swipe/index.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="home-swipe">
     <van-swipe class="my-swipe" :autoplay="3000" indicator-color="white" :show-indicators="false" @change="changeTab">
-      <van-swipe-item><img src="./images/1.jpg" alt="" /></van-swipe-item>
-      <van-swipe-item><img src="./images/2.jpg" alt="" /></van-swipe-item>
-      <van-swipe-item><img src="./images/3.jpg" alt="" /></van-swipe-item>
+      <van-swipe-item><img src="./images/bg1.jpg" alt="" /></van-swipe-item>
+      <van-swipe-item><img src="./images/bg2.jpg" alt="" /></van-swipe-item>
+      <van-swipe-item><img src="./images/bg3.jpg" alt="" /></van-swipe-item>
     </van-swipe>
     <div class="indicator">
       <span v-for="(item, index) in 3" :key="index" :class="index === currentIndex ? 'on' : ''"></span>
