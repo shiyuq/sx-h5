21a5360c4b00c44355fd412942bee6b9433765db
hww <71560358+heweiwei1@users.noreply.github.com>
2021-10-09 14:48:34 +0800

    Lyj h5 (#1)
    * 📦 新闻动态页面
    
    * 冲突解决&&新闻动态

--- src/views/news/component.js
+++ src/views/news/component.js
@@ -1,13 +1,41 @@
+import newsService from '../../api/news-service'
 export default {
   data() {
     return {
+      news: [],
+      currentPage: 1,
+      totalCount: 0,
+      loading: false,
+      finished: false
     }
   },
   mounted() {
   },
+  created() {
+    this.getData()
+<<<<<<< Updated upstream
+    console.log(`news1111111111`, this.news)
+=======
+>>>>>>> Stashed changes
+  },
   methods: {
     gotoMenu() {
       this.$router.go(-1)
+    },
+    loadMore() {
+      this.currentPage += 1
+      this.getData({ offset: (this.currentPage - 1) * 10 })
+    },
+    async getData(params = {}) {
+      this.loading = true
+      const { limit = 10, offset = 0 } = params
+      const { data } = await newsService.getNewsList({ limit, offset })
+      this.totalCount = data & data.totalCount
+      this.news = (data.rows || [])
+      if (this.totalCount <= this.news.length) {
+        this.finished = true
+      }
+      this.loading = false
     }
   }
 }
--- src/views/newsDetail/component.js
+++ src/views/newsDetail/component.js
@@ -1,13 +1,30 @@
+import newsService from '../../api/news-service'
+
 export default {
   data() {
     return {
+      id: '',
+      news: null
     }
   },
-  mounted() {
+
+  created() {
+    this.id = this.$route.params.id || ''
+    this.getData()
   },
+
+  computed: {},
+
+  mounted() {},
+
   methods: {
     gotoMenu() {
       this.$router.go(-1)
+    },
+
+    async getData() {
+      const { data } = await newsService.getNewsDetail({ id: this.id })
+      this.news = data || null
     }
   }
 }
