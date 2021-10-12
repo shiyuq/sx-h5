80d2035dc9dc43834991a8f5ec0e80f24e964970
Lu Yijun <xenia310lyj@gmail.com>
2021-10-12 15:54:34 +0800

    图片替换

---
+++ src/components/recommendRoute/component.js
@@ -0,0 +1,40 @@
+export default {
+  name: 'TabBar',
+  props: {
+    defaultActive: {
+      type: Number,
+      default: 0
+    },
+    data: {
+      type: Array,
+      default: () => {
+        return []
+      }
+    }
+  },
+  data() {
+    return {
+      active: this.defaultActive
+    }
+  },
+  watch: {
+    '$route': 'changeActive'
+  },
+  methods: {
+    handleChange(value) {
+      this.$emit('change', value)
+    },
+    changeActive(val) {
+      if (val.path === '/') {
+        this.active = 0
+      } else {
+        const index = this.data.findIndex(item => val.path.indexOf(item.to) > -1)
+        if (index >= 0) {
+          this.active = index
+        } else {
+          this.active = 0
+        }
+      }
+    }
+  }
+}
