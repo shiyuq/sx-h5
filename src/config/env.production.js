2f2adbac6a898a661add094d1559bda7f8a18847
shiyuq <1543082564@qq.com>
2021-10-14 10:05:23 +0800

    修改 api

--- src/config/env.production.js
+++ src/config/env.production.js
@@ -5,6 +5,6 @@ module.exports = {
   baseApi: 'https://www.xxx.com/api', // 正式api请求地址
   APPID: 'xxx',
   APPSECRET: 'xxx',
-  customerService: 'http://42.192.173.195:7002',
+  customerService: 'http://api.359peixun.cn/',
   $cdn: 'https://www.sunniejs.cn/static'
 }
--- src/config/env.staging.js
+++ src/config/env.staging.js
@@ -4,6 +4,6 @@ module.exports = {
   baseApi: 'https://test.xxx.com/api', // 测试api请求地址
   APPID: 'xxx',
   APPSECRET: 'xxx',
-  customerService: 'http://42.192.173.195:7002',
+  customerService: 'http://api.359peixun.cn/',
   $cdn: 'https://www.sunniejs.cn/static'
 }
